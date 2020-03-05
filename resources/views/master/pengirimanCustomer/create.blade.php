78468e067615d70341564a7fa6231e28e4ea9fc4
williamg47 <wgondowidjaja@gmail.com>
2020-03-05 20:30:49 +0700

    addition comment for neat coding

--- resources/views/master/pengirimanCustomer/create.blade.php
+++ resources/views/master/pengirimanCustomer/create.blade.php
@@ -5,11 +5,11 @@
 @endsection
 
 @section('title')
-    Create Kantor
+    Pengiriman Customer
 @endsection
 
 @section('subtitle')
-Page ini adalah untuk menambah kantor baru.
+Page ini adalah untuk menambah data pengiriman customer.
 @endsection
 
 @section('content')
@@ -29,35 +29,23 @@ Page ini adalah untuk menambah kantor baru.
                 <form novalidate class="needs-validation" method="post" action="/admin/pengirimanCustomer/store" enctype="multipart/form-data">
                 @csrf
                     <div class="form-row">
-                        <div class="col-md-12">
-                            <div class="position-relative form-group">
-                                <label class="">Alamat</label>
-                                <textarea style="resize: none;" rows="5" oninput="let p = this.selectionStart; this.value = this.value.toUpperCase();
-                                this.setSelectionRange(p, p);" style="text-transform:uppercase" name="alamat"
-                                placeholder="ALAMAT" type="text" class="form-control" required></textarea>
-                                <div class="invalid-feedback">
-                                    Mohon inputkan alamat valid.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="col-md-5">
+                        <div class="col-md-4">
                             <div class="position-relative form-group">
                                 <label class="">ID Pengiriman Customer</label>
                                 <input oninput="let p = this.selectionStart; this.value = this.value.toUpperCase();
-                                this.setSelectionRange(p, p);" style="text-transform:uppercase" name="id_pengiriman_customer" id="" 
+                                this.setSelectionRange(p, p);" style="text-transform:uppercase" name="id_pengiriman_customer" 
                                 type="text" class="form-control" value="{{$nextId}}" disabled>
                             </div>
                         </div>
                     </div>
                     <div class="form-row">
-                        <div class="col-md-3">
+                        <div class="col-md-4">
                             <div class="position-relative form-group">
-                                <label class="">Jenis Kantor</label>
-                                <select name="is_warehouse" class="form-control">
-                                    <option value="0" class="form-control">KANTOR CABANG</option>
-                                    <option value="1" class="form-control">WAREHOUSE</option>
+                                <label class="">Kurir Customer</label>
+                                <select name="kurir" class="form-control">
+                                    @foreach ($allKurir as $kurir)
+                                        <option value="{{$kurir->id}}" class="form-control">{{$kurir->nama . ' (' . $kurir->nopol . ')'}}</option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
@@ -65,36 +53,12 @@ Page ini adalah untuk menambah kantor baru.
                     <div class="form-row">
                         <div class="col-md-4">
                             <div class="position-relative form-group">
-                                <label class="">Kota</label>
+                                <label class="">Kantor Asal</label>
                                 <select name="kota" class="form-control">
-                                    {{-- @foreach ($listKota as $i)
-                                        <option class="form-control" value="{{$i}}">{{$i}}</option>
-                                    @endforeach --}}
+                                    @foreach ($allKantor as $kantor)
+                                        <option class="form-control" value="{{$kantor->id}}">{{$kantor->alamat}}</option>
+                                    @endforeach
                                 </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="col-md-2">
-                            <div class="position-relative form-group">
-                                <label class="">Longitude</label>
-                                <input required oninput="let p = this.selectionStart; this.value = this.value.toUpperCase();
-                                this.setSelectionRange(p, p);" style="text-transform:uppercase" name="longitude" 
-                                placeholder="0.000000" step="0.000001" type="number" max="999.999999" min="0.000000" class="form-control">
-                                <div class="invalid-feedback">
-                                    Mohon inputkan longitude valid. Longitude untuk kantor bisa di search di web.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="position-relative form-group">
-                                <label class="">Latitude</label>
-                                <input required oninput="let p = this.selectionStart; this.value = this.value.toUpperCase();
-                                this.setSelectionRange(p, p);" style="text-transform:uppercase" name="latitude" 
-                                placeholder="0.000000" step="0.000001" type="number" max="999.999999" min="0.000000" class="form-control">
-                                <div class="invalid-feedback">
-                                    Mohon inputkan latitude valid. Latitude untuk kantor bisa di search di web.
-                                </div>
                             </div>
                         </div>
                     </div>
