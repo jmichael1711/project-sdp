16a8aa0efccbf77ba841616e42e94104fe7a05d8
Heinrich9029 <53186136+Heinrich9029@users.noreply.github.com>
2020-05-11 15:59:51 +0700

    add selesai resi dari kantor

--- routes/web.php
+++ routes/web.php
@@ -207,6 +207,7 @@ Route::group(['middleware' => ['checkstatus:kasir']], function () {
     Route::post('/admin/resi/isiSejarah', 'ResiController@isiSejarah');
     Route::get('/admin/resi/trackingField/{id}', 'ResiController@trackingField');
     Route::get('/admin/resi/print/{id}', 'ResiController@print');
+    Route::get('/admin/resi/selesai/{id}', 'ResiController@selesai');
     
     //ADMING - BON MUAT
     Route::get('/admin/bonmuat/create', 'Bon_MuatController@create');
